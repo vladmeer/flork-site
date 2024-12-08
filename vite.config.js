a6fed715907b515d45f85f8a51923df8f9270997
vladmeer <davevasugod@gmail.com>
2024-12-08 15:59:05 -0400

    Update Vite configuration and package.json for enhanced preview and server settings
    - Modified the "preview" script in package.json to allow hosting on all network interfaces (0.0.0.0).
    - Updated vite.config.js to configure both preview and server settings with host and port options, ensuring consistent behavior across environments.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+    strictPort: true
+  },
+  server: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+    strictPort: true
+  }
 })
