0c3056d67a478d495b525a7fb763559241a2d1c9
vladmeer <davevasugod@gmail.com>
2025-01-12 20:31:51 -0400

    Enhance Preloader component with new background image and updated styles
    - Added a new background image to the Preloader component for improved aesthetics.
    - Introduced a background wrapper in Preloader.jsx to position the background image.
    - Updated Preloader.module.css to include styles for the background wrapper and image, enhancing the visual appeal and layout.
    - Adjusted z-index values to ensure proper layering of preloader content.

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import styles from './Preloader.module.css';
 import florkCargando from '../../assets/images/new/flork_loading.svg';
 import loaderGif from '../../assets/images/new/gif_loader.gif';
+import backgroundImage from '../../assets/images/new/background.png';
 
 const Preloader = () => {
   return (
     <div className={styles.preloaderContainer}>
+      <div className={styles.backgroundWrapper}>
+        <img
+          className={styles.backgroundImage}
+          decoding="async"
+          sizes="100vw"
+          src={backgroundImage}
+          alt="Background"
+        />
+      </div>
       <div className={styles.preloaderContent}>
         <img src={florkCargando} alt="Flork cargando" className={styles.florkImage} />
         <div className={styles.spinnerContainer}>
