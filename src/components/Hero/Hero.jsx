7c38aa98e906deb5721046d680aa7d804826fa61
vladmeer <davevasugod@gmail.com>
2025-01-12 20:26:24 -0400

    Refactor AIAgent and Hero components with updated styles and assets
    - Replaced hardcoded colors in AIAgent.module.css with CSS variables for improved maintainability.
    - Updated background and border styles in AIAgent to enhance visual consistency.
    - Integrated a new background image in Hero component for better aesthetics.
    - Adjusted Hero.module.css to improve layout, including overflow handling and image rendering properties.
    - Enhanced responsiveness and visual appeal of the Hero section with updated CSS properties.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import FlorkWantsYou from '../FlorkWantsYou/FlorkWantsYou'
 import ContractInfo from '../ContractInfo/ContractInfo'
 import Florkonomics from '../Florkonomics/Florkonomics'
 import FlorkWaiting from '../FlorkWaiting/FlorkWaiting'
+import backgroundImage from '../../assets/images/new/background.png'
 
 
 
@@ -16,16 +17,13 @@ const Hero = () => {
   return (
     <div className={styles.heroContainer}>
       <div className={styles.backgroundWrapper}>
-        {/* <img
+        <img
           className={styles.backgroundImage}
           decoding="async"
           sizes="100vw"
-          srcSet="https://framerusercontent.com/images/NpOIKJhTZgXWJ73lO8DouffVEZ0.jpg?scale-down-to=2048 553w,
-                  https://framerusercontent.com/images/NpOIKJhTZgXWJ73lO8DouffVEZ0.jpg?scale-down-to=4096 1106w,
-                  https://framerusercontent.com/images/NpOIKJhTZgXWJ73lO8DouffVEZ0.jpg 1441w"
-          src="https://framerusercontent.com/images/NpOIKJhTZgXWJ73lO8DouffVEZ0.jpg"
+          src={backgroundImage}
           alt="Background"
-        /> */}
+        />
       </div>
       <div className={styles.contentWrapper}>
         <h1 className={styles.title}>
